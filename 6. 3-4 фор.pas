bc5b64b8ef0d5986bdc3e37a65d77810a749cc50
0renessans0 <144677876+0renessans0@users.noreply.github.com>
2023-10-04 21:28:16 +0300

    Add files via upload

---
+++ 6. 3-4 фор.pas
@@ -0,0 +1,12 @@
+﻿Var n, i, sum: integer; 
+Begin
+writeln ('введите число');
+readln(n); 
+writeln('Делители числа:'); 
+for i := 1 to n div 2 do 
+if (n mod i) = 0 then 
+begin 
+write(i,' '); 
+end; 
+writeln(n); 
+End.
